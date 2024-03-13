d2d23dc0f8eb823ae01f37275258a62ee419fd8f
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2024-03-13 20:18:09 +0000

    add client page

---
+++ my-react-app/src/pages/client.jsx
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet-async';
+
+// import { ClientView } from 'src/sections/client/view';
+
+// ----------------------------------------------------------------------
+
+export default function UserPage() {
+  return (
+    <>
+      <Helmet>
+        <title> Clientes </title>
+      </Helmet>
+
+      {/* <ClientView /> */}
+    </>
+  );
+}
