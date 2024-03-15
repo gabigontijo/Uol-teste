56189948a7d9b99d5107b96ad283893d9e5df7a8
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2024-03-15 19:36:34 +0000

    add new form client and ajustment state

---
+++ my-react-app/src/sections/client/form-new-client.jsx
@@ -0,0 +1,269 @@
+import { useState } from 'react';
+import PropTypes from "prop-types";
+// import { useQuery } from "react-query";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from "@mui/material/TextField";
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+// import Autocomplete from '@mui/material/Autocomplete';
+
+// import { createClient, updateClient } from 'src/apis/client';
+
+import Typography from "@mui/material/Typography";
+
+import MaskFields from "./mask-field";
+import { clientInterface } from './view/type';
+// import { clientInterface } from './view/type';
+// import SelectPixFields from '../common/input-select-pix';
+// import InputFileUpload from '../common/input-upload-file';
+
+// ----------------------------------------------------------------------
+
+const statusCurrencies = [
+    {
+        value: 1,
+        label: "Ativo",
+    },
+    {
+        value: 2,
+        label: "Inativo",
+    },
+    {
+        value: 3,
+        label: "Aguardando ativação",
+    },
+    {
+        value: 4,
+        label: "Desativado",
+    },
+];
+
+export default function FormNewClient({
+    setNewClient,
+    setAlert,
+    setAlertError,
+    setNextStep,
+    setMessageError,
+    setMessageAlert,
+    clientId,
+    setClientId,
+    refetchClients,
+    // stateClient: client,
+    setStateClient,
+    setRenderForm,
+}) {
+
+    const [client, setClient] = useState(clientInterface);
+
+      const handleSubmit = async () => {
+        try {
+          const bodyClient = {
+            name: client.name,
+            email: client.email,
+            phone: client.phone,
+            cpf: client.cpf,
+            status: client.status,
+          };
+        //   await createClient(bodyClient);
+        //   setAlert(true);
+        //   setMessageAlert('Cliente cadastrado com sucesso');
+        //   setNewUser(false);
+        //   setClient(clientInterface)
+        //   refetchClients();
+        console.log('bodyClient', bodyClient)
+        console.log('state client', client)
+        } catch (error) {
+        //   // eslint-disable-next-line no-debugger
+        //   debugger;
+        //   setAlertError(true);
+        //   setMessageError('Erro ao Cadastrar o cliente');
+          console.log('Erro ao Cadastrar o cliente:', error);
+        }
+      };
+
+      const handleSubmitEdit = async () => {
+        // try {
+        //   // eslint-disable-next-line no-debugger
+        //   debugger;
+        //   const nonEmptyState = Object.fromEntries(
+        //     Object.entries(client).map(([key, value]) => [key, value || ''])
+        //   );
+        //   const bodyClientEdit = {
+        //     name: nonEmptyState.name,
+        //     pixType: nonEmptyState.pixType,
+        //     pixKey: nonEmptyState.pixKey,
+        //     partnerId: Number(nonEmptyState.partner.id),
+        //     phone: nonEmptyState.phone,
+        //     cpf: nonEmptyState.cpf,
+        //     documents: '',
+        //   };
+        //   const response = await updateClient(bodyClientEdit, clientId);
+        //   console.log('Resposta da API:', response);
+        //   setNewUser(false);
+        //   setClientId(null);
+        //   setAlert(true);
+        //   setMessageAlert('Cliente editado com sucesso');
+        //   setStateClient(clientInterface)
+        //   refetchClients();
+        // } catch (error) {
+        //   setAlertError(true);
+        //   setMessageError('Erro ao Editar o cliente');
+        //   setNewUser(true);
+        //   console.log('Erro ao Editar o cliente:', error);
+        // }
+      };
+
+    const handleBack = () => {
+        setClientId(null);
+        setClient(clientInterface);
+        setRenderForm(false);
+    }
+
+    const handleClientChange = (event) => {
+        const { name, value } = event.target;
+        setClient({
+          ...client,
+          [name]: value,
+        });
+      };
+
+    return (
+        <>
+            <Stack spacing={{ xs: 1, sm: 2 }}>
+                <Box width="100%">
+                    <Typography mt={4} variant="subtitle1">
+                       {clientId === null  ? 'Novo usário' : 'Editar usuário'} 
+                    </Typography>
+                    <Typography mb={4} variant="body1">
+                    {clientId === null  ? 'Informe os campos a seguir para criar novo usuário' : 'Informe os campos a seguir para editar o usuário'} 
+                    </Typography>
+                </Box>
+                <Stack direction="column" spacing={{ xs: 1, sm: 2 }} width="40%">
+                    <Box width="100%">
+                        <TextField
+                            name="name"
+                            label="Nome"
+                            type="text"
+                              value={client.name}
+                              onChange={handleClientChange}
+                            fullWidth
+                        />
+                    </Box>
+                    <Box width="100%">
+                        <TextField
+                            name="email"
+                            label="E-mail"
+                            type="text"
+                              value={client.email}
+                              onChange={handleClientChange}
+                            fullWidth
+                        />
+                    </Box>
+                    <Box width="100%">
+                        <MaskFields
+                            mask="999.999.999-99"
+                            name="cpf"
+                            label="CPF"
+                            type="text"
+                          value={client.cpf}
+                          handleChange={handleClientChange}
+                        />
+                    </Box>
+                    <Box width="100%">
+                        <MaskFields
+                            mask="(99) 99999-9999"
+                            name="phone"
+                            label="Telefone"
+                            type="text"
+                          value={client.phone}
+                          handleChange={handleClientChange}
+                        />
+                    </Box>
+                    <Box component="form" noValidate autoComplete="off">
+                        <div>
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={client.status}
+                                    label="Status"
+                                    name='status'
+                                    onChange={handleClientChange}
+                                >
+                                    {statusCurrencies.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                                </Select>
+                            </FormControl>
+                        </div>
+                    </Box>
+                </Stack>
+            </Stack>
+            <Stack
+                direction="row"
+                alignItems=""
+                justifyContent="space-between"
+                mt={10}
+                width="40%"
+            >
+                {clientId === null && ( 
+                <Button
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                    color="success"
+                    onClick={handleSubmit}
+                    sx={{ width: "45%" }}
+                >
+                    Criar
+                </Button>
+                )}
+                {clientId !== null && ( 
+                <Button
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                    color="success"
+                   onClick={handleSubmitEdit}
+                    sx={{ width: "45%" }}
+                >
+                    Editar
+                </Button>
+                )}
+                <Button
+                    size="large"
+                    type="submit"
+                    variant="outlined"
+                    color="inherit"
+                    sx={{ width: "45%" }}
+                    onClick={handleBack}
+                >
+                    Voltar
+                </Button>
+            </Stack>
+        </>
+    );
+}
+
+FormNewClient.propTypes = {
+    setNewClient: PropTypes.func,
+    setAlert: PropTypes.func,
+    setAlertError: PropTypes.func,
+    setNextStep: PropTypes.func,
+    setMessageError: PropTypes.func,
+    setMessageAlert: PropTypes.func,
+    setClientId: PropTypes.func,
+    clientId: PropTypes.any,
+    refetchClients: PropTypes.func,
+    // stateClient: PropTypes.any,
+    setStateClient: PropTypes.func,
+    setRenderForm: PropTypes.func,
+};
