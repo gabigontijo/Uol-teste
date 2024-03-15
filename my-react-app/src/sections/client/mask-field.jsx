9cb4f25b85ae70909a83b200a8ca4b4dafb1d938
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2024-03-15 18:35:07 +0000

    add mask_field

---
+++ my-react-app/src/sections/client/mask-field.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import InputMask from 'react-input-mask';
+
+import TextField from '@mui/material/TextField';
+
+export default function MaskFields({ mask, name, label, type, value, handleChange }) {
+  return (
+    <InputMask
+      mask={mask}
+      value={value}
+      onChange={(event) => handleChange({ target: { name, value: event.target.value } })}
+    >
+      {(inputProps) => (
+        <TextField {...inputProps} name= {name} label={label} type={type} fullWidth />
+      )}
+    </InputMask>
+  );
+}
+
+MaskFields.propTypes = {
+  mask: PropTypes.string,
+  name: PropTypes.string,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.any,
+  handleChange: PropTypes.func,
+};
